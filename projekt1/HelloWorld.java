3d764290d5392e44671233b927b9bfb69cfcedc9
karlwast <karl@wastvind.com>
2022-08-22 17:51:04 +0200

    supertets

--- projekt1/HelloWorld.java
+++ projekt1/HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hejsna");
         System.out.println("testets");
+        System.out.println("hejehejeh");
+        System.out.println("hejehejeh");
+
+        System.out.println("hejehejeh");
+
     }
 }
