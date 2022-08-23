158473e1b8bbf91179ba55aac3437acb6505b956
karlwastvind <95290252+karlwastvind@users.noreply.github.com>
2022-08-23 11:02:32 +0200

    Update HelloWorld.java

--- projekt1/HelloWorld.java
+++ projekt1/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
         System.out.println("hejsna");
         System.out.println("testets");
         System.out.println("hejehejeh");
-        System.out.println("hejehejeh");
+        System.out.println("github");
+        System.out.println("github");
 
         for (int i = 0; i < 10; i++) {
             System.out.println("karl");
