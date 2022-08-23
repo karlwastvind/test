c7e61d1b84a3af91e42275ecda34f188c8b76d19
karlwast <karl@wastvind.com>
2022-08-23 11:05:33 +0200

    mannenmanenn

--- projekt1/HelloWorld.java
+++ projekt1/HelloWorld.java
@@ -1,14 +1,9 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hejsna");
-        System.out.println("testets");
-        System.out.println("hejehejeh");
-        System.out.println("github");
-        System.out.println("github");
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("karl");
+            System.out.println("mannenmanenn");
         }
 
     }
